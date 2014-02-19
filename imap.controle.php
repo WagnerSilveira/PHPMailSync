a8974f566e3025088120ecbb16e7d219b36b11e4
Wagner Silveira <wagnerhsilveira@gmail.com>
2014-02-19 12:18:42 -0300

    Update imap.controle.php

--- imap.controle.php
+++ imap.controle.php
@@ -5,19 +5,16 @@ error_reporting();
 $origem= new Imap(string host, string usuario ,string senha,string tipo, bool ssl);
 $destino=  new Imap(string host, string usuario ,string senha,string tipo, bool ssl);
 
-
 if(!$origem->testarConexao()){
 	echo "Nao foi possivel conectar o servidor de origem, o servidor nao respondeu atraves do endereco:<strong> $origem->servidor </strong> na porta <strong> $origem->porta </strong>";
 	exit;
 }
 if(!$origem->conectar()){
 	echo "Falha de autenticacao no servidor de origem: $origem->servidor com a conta $origem->usuario <br />";
-
 	exit;
 }
 if(!$destino->testarConexao()){
 	echo "Nao foi possivel conectar o servidor de destino, o servidor nao respondeu atraves do endereco:<strong>  $destino->servidor </strong> na porta <strong>  $destino->porta </strong>";
-
 	exit;
 }
 if(!$destino->conectar()){
@@ -25,4 +22,26 @@ if(!$destino->conectar()){
 	exit;
 }
 
+
+
+$destino->listarMailBox();
+$destino->verificarTipoSeparador();
+
+echo '<pre>';
+
+ foreach($origem->listarMailBox() as $mailbox){
+	$pastasOrigem=$origem->listarPastas($mailbox);
+	echo $destino->criarMailboxInexistentes($origem,$pastasOrigem);
+	//$origem->listarMensagensPorPastas($pastasOrigem);
+	//echo $origem->verificarPadraoMailbox($origem,$pastasOrigem);
+	echo "$pastasOrigem \n";
+	if($destino->verificarMensagensDuplicadas($origem,$pastasOrigem)){
+		foreach ($destino->verificarMensagensDuplicadas($origem,$pastasOrigem) as $uid){
+			echo $destino->migrarMensagensImap($origem,$pastasOrigem,$uid).;
+		}
+	}
+//echo $origem->listarTotalMensagensPorMailbox($pastas)."\n";
+} 
+
+echo '</pre>';
 ?>  
