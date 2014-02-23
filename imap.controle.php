cc95b1314d5ab72f6a5cdb4381073d06fc04aae8
Wagner Silveira <wagnerhsilveira@gmail.com>
2014-02-23 04:08:48 -0300

    Script de execução em linha de comando
    No momento a aplicação está pronta para a migração entre contas IMAP->IMAP
    Futuramente terá IMAP->POP, POP->IMAP, POP->POP.
    
    Como utilizar: 
    
    php script.php --host1 SERVIDOR_EMAIL_ORIGEM --usuario1 CONTA@DOMINIO --senha1 SENHA_DA_CONTA_ORIGEM  --tipo1 (imap ou pop3) --ssl1 (0 ou 1) --host2  --usuario2 CONTA@DOMINIO --senha2 SENHA_DA_CONTA_DESTINO --tipo2 (imap ou pop3) --ssl2 (0 ou 1)

--- imap.controle.php
+++ imap.controle.php
@@ -1,52 +1,59 @@
 <?php
 include('Imap.class.php');
-error_reporting();
+$parametros= array('host1:','usuario1:','senha1:','tipo1:','ssl1:','host2:','usuario2:','senha2:','tipo2:','ssl2:');
+$argumentos=getopt(null,$parametros); 
 
 //$origem= new Imap(string host, string usuario ,string senha,string tipo, bool ssl);
 //$destino=  new Imap(string host, string usuario ,string senha,string tipo, bool ssl);
 
+$origem = new Imap($argumentos['host1'],$argumentos['usuario1'],$argumentos['senha1'],$argumentos['tipo1'],$argumentos['ssl1']);
+$destino= new Imap($argumentos['host2'],$argumentos['usuario2'],$argumentos['senha2'],$argumentos['tipo2'],$argumentos['ssl2']);
+
 
 if(!$origem->testarConexao()){
-	echo "Nao foi possivel conectar o servidor de origem, o servidor nao respondeu atraves do endereco:<strong> $origem->servidor </strong> na porta <strong> $origem->porta </strong>";
+	echo "Nao foi possivel conectar o servidor de origem, o servidor nao respondeu atraves do endereco: $origem->servidor  na porta  $origem->porta \n";
 	exit;
 }
 if(!$origem->conectar()){
-	echo "Falha de autenticacao no servidor de origem: $origem->servidor com a conta $origem->usuario <br />";
+	echo "Falha de autenticacao no servidor de origem: $origem->servidor com a conta $origem->usuario \n";
 	exit;
 }
 if(!$destino->testarConexao()){
-	echo "Nao foi possivel conectar o servidor de destino, o servidor nao respondeu atraves do endereco:<strong>  $destino->servidor </strong> na porta <strong>  $destino->porta </strong>";
+	echo "Nao foi possivel conectar o servidor de destino, o servidor nao respondeu atraves do endereco: $destino->servidor na porta $destino->porta \n";
 	exit;
 }
 if(!$destino->conectar()){
-	echo "Falha de autenticacao no servidor de destino: $destino->servidor com a conta $destino->usuario <br />";
+	echo "Falha de autenticacao no servidor de destino: $destino->servidor com a conta $destino->usuario \n";
 	exit;
 }
 
 
-
+$stream= fopen("php://output",'w');
 $destino->listarMailBox();
 $destino->verificarTipoSeparador();
 
-echo "<pre> \n";
  foreach($origem->listarMailBox() as $mailbox){
 	$pastasOrigem=$origem->listarPastas($mailbox);
-    echo $destino->criarMailboxInexistentes($origem,$pastasOrigem);
-
+	 fwrite($stream, $destino->criarMailboxInexistentes($origem,$pastasOrigem));
+	 $destino->limparImapCache($origem);
+	 gc_collect_cycles();
+	 gc_disable();
 }
 
  foreach($origem->listarMailBox() as $mailbox){
 	$pastasOrigem=$origem->listarPastas($mailbox);
-	
-     echo "Verificando conteudo na pasta $pastasOrigem \n";
+
+     fwrite($stream, "Verificando conteudo na pasta $pastasOrigem \n");
 	if($destino->verificarMensagensDuplicadas($origem,$pastasOrigem)){
+		
 		foreach ($destino->verificarMensagensDuplicadas($origem,$pastasOrigem) as $uid){
-	         echo $destino->migrarMensagensImap($origem,$pastasOrigem,$uid);
+			 fwrite($stream, $destino->migrarMensagensImap($origem,$pastasOrigem,$uid));
+			 $destino->limparImapCache($origem);
+			 gc_enable();
+
 		}
+		gc_collect_cycles();
+		gc_disable();
 	}
-
 } 
- 
-
-echo '</pre>';
 ?>  
