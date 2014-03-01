8741f5274b00e1315d071e5665389b80e294ad5d
Wagner <wagner@ubuntu.wagnersilveira.kinghost.net>
2014-03-01 17:48:22 -0300

    Ajustada Descrição de autor e copyright
    Committer: wagner852 <wagnerhsilveira@gmail.com>
    	modified:   Imap.class.php
    	modified:   imap.controle.php

--- Imap.class.php
+++ Imap.class.php
@@ -1,21 +1,21 @@
+<?php
 /*
  * Direitos Autorais (C) 2014 Wagner Hahn Silveira.
  *
  * Autor:
- *	Wagner Hahn Silveira <wagnerhsilveira@gmail.com>
+ *      Wagner Hahn Silveira <wagnerhsilveira@gmail.com>
  *
  * Este software é licenciado sob os termos da Licença Pública Geral GNU
  * License versão 2, como publicada pela Fundação de Software Livre, e
  * pode ser copiada, distribuida, e modificada sob estes termos.
  *
  * Este programa é distribuido na esperança que será util,
- * mas SEM NENHUMA GARANTIA; sem mesmo a garantia implícita de 
- * COMERCIALIZAÇÃO ou de ADEQUAÇÃO A UM DETERMINADO FIM. veja o 
+ * mas SEM NENHUMA GARANTIA; sem mesmo a garantia implícita de
+ * COMERCIALIZAÇÃO ou de ADEQUAÇÃO A UM DETERMINADO FIM. veja o
  * Licença Pública Geral GNU para obter mais detalhes.
  *
  */
 
-<?php
 class Imap{
 	//Conexao 
 	private $stream;
--- imap.controle.php
+++ imap.controle.php
@@ -3,18 +3,19 @@
  * Direitos Autorais (C) 2014 Wagner Hahn Silveira.
  *
  * Autor:
- *	Wagner Hahn Silveira <wagnerhsilveira@gmail.com>
+ *      Wagner Hahn Silveira <wagnerhsilveira@gmail.com>
  *
  * Este software é licenciado sob os termos da Licença Pública Geral GNU
  * License versão 2, como publicada pela Fundação de Software Livre, e
  * pode ser copiada, distribuida, e modificada sob estes termos.
  *
  * Este programa é distribuido na esperança que será util,
- * mas SEM NENHUMA GARANTIA; sem mesmo a garantia implícita de 
- * COMERCIALIZAÇÃO ou de ADEQUAÇÃO A UM DETERMINADO FIM. veja o 
- * Licença Pública Geral GNU para obter mais detalhes.
+ * mas SEM NENHUMA GARANTIA; sem mesmo a garantia implícita de
+ * COMERCIALIZAÇÃO ou de ADEQUAÇÃO A UM DETERMINADO FIM. veja o
+ * Licença Pública Geral GNU para obter mais detalhes.
  *
  */
+
 if(extension_loaded('imap')){
 include('Imap.class.php');
 if (php_sapi_name()=='cli'){
