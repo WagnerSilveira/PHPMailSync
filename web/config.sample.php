1a3e9e8c29192427dd7eb59972dcb7b6f8bed38d
wagner852 <wagnerhsilveira@gmail.com>
2014-03-06 09:18:07 -0300

    modificado: .gitignore 	novo arquivo: web/config.sample.php 	modificado: Imap.class.php

---
+++ web/config.sample.php
@@ -0,0 +1,15 @@
+<?php
+class Config{
+ 
+        public $servidorSmtp= '';
+        public $emailRementente = '';
+        public $senhaEmail = '';
+       
+       /*********************/
+       public $driver= 'mysql';
+       public $servidorBd= '';
+       public $usuarioBd = '';
+       public $bancoDeDados = '';
+       public $senhaBd = '';
+}
+?>
