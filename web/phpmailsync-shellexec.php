f401bb48bf8476eb64a1cc094d9d625d5fcb45ee
Wagner Silveira <wagnerhsilveira@gmail.com>
2014-03-13 14:43:31 -0300

    Separando versão web

--- web/phpmailsync-shellexec.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo shell_exec("php PHPMailSync/imap.controle.php --host1 ".$_GET['host1']." --usuario1 ".$_GET['usuario1']."  --senha1 ".$_GET['senha1']."  --tipo1 ".$_GET['tipo1'] ." --host2 ".$_GET['host2'] ."  --usuario2 ".$_GET['usuario2'] ."  --senha2 ".$_GET['senha2'] ." > ".$_GET['log'].".txt ");
-?>
